17035456dd811d0beed95c255a879573fa051904
eduresser <eduardo.resser@pareto.io>
2025-11-21 20:09:26 -0300

    change logging

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ def setup_logging(log_level: str = "INFO"):
     logging.basicConfig(
         level=numeric_level,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt='%Y-%m-%d %H:%M:%S',
+        force=True
     )
 
 
